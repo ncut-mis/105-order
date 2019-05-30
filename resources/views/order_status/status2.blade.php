490c8e7ad9d56aba7e69403fd77f0a0bacbe0f39
3A532081 <tanshui601@gmail.com>
2019-05-31 01:35:57 +0800

    加入結帳後頁面

--- resources/views/order_status/status2.blade.php
+++ resources/views/order_status/status2.blade.php
@@ -3,7 +3,7 @@
 <font color="#000000" size="6" face="微軟正黑體"><b>餐點已全數完成 </b></font><br>
 <font color="#000000" size="6" face="微軟正黑體"><b>★ 請慢用 ★ </b></font>
 <br>
-<form method="get" action="/checkout">
+<form method="get" action="{{route('order.checkout')}}">
 {{ csrf_field() }}
     <button type="submit" class="btn btn-primary">
         <i ></i>我要結帳
